539a2b012f734c49cf536b504ab89c351dc944c4
Peter Ruibal <ruibalp@gmail.com>
2011-11-05 00:24:18 -0700

    TestCSSLexer:  Print Token names in token stream

--- java/TestCSSLexer.java
+++ java/TestCSSLexer.java
@@ -1,8 +1,20 @@
 import java.io.*;
+import java.lang.reflect.Field;
 import org.antlr.runtime.*;
 
 
 public class TestCSSLexer {
+
+    public static String getTokenName(int n) {
+        for (Field f : CSSLexer.class.getDeclaredFields()) {
+            try {
+                if (f.getInt(null) == n) {
+                    return f.getName();
+                }
+            } catch (IllegalAccessException ex) { }
+        }
+        return "Unknown";
+    }
 
     public static void main(String args[]) throws Exception {
         int code = 0;
@@ -12,7 +24,9 @@ public class TestCSSLexer {
             Token token;
             do {
                 token = lex.nextToken();
-                System.out.println("Token: <" + token + ">");
+                System.out.println(
+                    "<" + getTokenName(token.getType()) +
+                    ": " + token + ">");
             } while (token.getType() != Token.EOF);
         }
         System.exit(code);
