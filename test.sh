0f3725185cd3e54ad139167db28966365bfc7ef8
Peter Ruibal <ruibalp@gmail.com>
2011-11-07 15:52:01 -0800

    Only test .css files (not vim swap files...)

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ echo "Compiling grammar..."
 ./build.sh
 
 echo "Testing Lexer..."
-for f in $(find example -type f); do
+for f in $(find example -type f -name '*.css'); do
   echo -n "$f: "
   ./java/TestCSSLexer $f 2>err.log 1>/dev/null
   ERRORS=$(cat err.log | wc -l)
