8ad7c1fe689056c8f19fc4e2bcde92cc81bf1661
Peter Ruibal <ruibalp@gmail.com>
2011-11-07 14:41:01 -0800

    Add a quick and dirty test script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+echo "Compiling grammar..."
+./build.sh
+
+echo "Testing Lexer..."
+for f in $(find example -type f); do
+  echo -n "$f: "
+  ./java/TestCSSLexer $f 2>err.log 1>/dev/null
+  ERRORS=$(cat err.log | wc -l)
+  if [ $ERRORS -gt 0 ]; then
+    echo "FAIL"
+    cat err.log
+  else
+    echo "OK"
+  fi
+done
