aaec33ed5314d923c69550fdeda4a1dd77a99989
Peter Ruibal <ruibalp@gmail.com>
2011-11-10 03:42:06 -0800

    catch unhandled exceptions in parse_verify

--- python/css_parse_verify.py
+++ python/css_parse_verify.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 try:
     from text.cssparser.CSSParser import CSSParser
     from text.cssparser.CSSLexer import CSSLexer
@@ -8,6 +9,8 @@ import antlr3
 from argparse import ArgumentParser
 import json
 import sys
+import logging
+import traceback
 
 
 class ErrorCapturingCSSParser(CSSParser):
@@ -37,22 +40,38 @@ class ErrorCapturingCSSParser(CSSParser):
 def main():
     ap = ArgumentParser()
     ap.add_argument('input_files', nargs='+')
-    ap.add_argument('--json', action='store_true')
+    ap.add_argument('--json', action='store_true',
+                    help="Output errors as json instead of to stderr")
+    ap.add_argument('--level', default='INFO',
+                    help="Log Level [%(default)s]")
 
     ns = ap.parse_args()
+
+    # Init Logging
+    logging.basicConfig()
+    logging.getLogger('').setLevel(getattr(logging, ns.level.upper()))
+
     n_errors = 0
     json_result = {}
     for file in ns.input_files:
-        f = antlr3.FileStream(file)
-        lexer = CSSLexer(f)
-        tokenStream = antlr3.CommonTokenStream(lexer)
-        parser = ErrorCapturingCSSParser(tokenStream,
-                                         emit_errors=(not ns.json))
-        parser.styleSheet()
-        n_errors += parser.getNumberOfSyntaxErrors()
-        
-        if ns.json:
-            json_result.setdefault(file, []).extend(parser.errors)
+        logging.getLogger('').debug("Processing %s", file)
+        try:
+            f = antlr3.FileStream(file)
+            lexer = CSSLexer(f)
+            tokenStream = antlr3.CommonTokenStream(lexer)
+            parser = ErrorCapturingCSSParser(tokenStream,
+                                             emit_errors=(not ns.json))
+            parser.styleSheet()
+            n_errors += parser.getNumberOfSyntaxErrors()
+
+            if ns.json:
+                json_result.setdefault(file, []).extend(parser.errors)
+        except Exception as e:
+            if ns.json:
+                msg = traceback.format_exc()
+                json_result.setdefault(file, []).append({'msg': msg})
+            else:
+                traceback.print_exc(file=sys.stderr)
 
     if ns.json:
         print json.dumps(json_result)
