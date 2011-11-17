c0eef8d8b81750a04fb3f3f4d1accdd19135bf43
Peter Ruibal <ruibalp@gmail.com>
2011-11-17 14:28:08 -0800

    python: Add a helper function for stringifying token names

--- python/parse_utils.py
+++ python/parse_utils.py
@@ -8,6 +8,8 @@ except ImportError:
     from CSSLexer import CSSLexer
 
 class ErrorCapturingRecognizer(antlr3.BaseRecognizer):
+    token_lookup = None
+
     def __init__(self, *args, **kwargs):
         self.errors = []
         self.emit_errors = kwargs.pop('emit_errors', True)
@@ -23,6 +25,20 @@ class ErrorCapturingRecognizer(antlr3.BaseRecognizer):
           'col': e.charPositionInLine,
           'msg': self.getErrorMessage(e, self.tokenNames)
         }
+
+    @classmethod
+    def token_to_name(cls, token):
+        if cls.token_lookup is None:
+            cls.token_lookup = {}
+            for k in dir(cls):
+                if k != k.upper():
+                    continue
+                v = getattr(cls, k)
+                if not isinstance(v, (int, long)):
+                    continue
+                cls.token_lookup[v] = k
+
+        return cls.token_lookup.get(token.getType(), 'UNKNOWN')
 
 class ErrorCapturingCSSLexer(ErrorCapturingRecognizer, CSSLexer):
     pass        
